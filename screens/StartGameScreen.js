075753833d2f45a70ca20ab72a666e1a8d7a2e98
Vishesh <VisheshSingh16@gmail.com>
2020-01-11 21:27:49 -0600

    Card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card';
+
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title='Reset' onPress={() => {}} />
           <Button title='Confirm' onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
